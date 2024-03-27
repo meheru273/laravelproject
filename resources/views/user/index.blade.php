a99e2f4aabc81c92d4dbe03db4e85d9a2d9b367e
meheru273 <meherujannat@gmail.com>
2024-03-27 22:49:39 +0600

    first commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.base')
+@section('content')
+<h1>user dashboard</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AppController;
+
+if( ini_get( 'wincache.ocenabled' ) )
+{
+    ini_set( 'wincache.ocenabled', '0' );
+}
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/',[AppController::class,'index'])->name('app.index');
+
+Route::middleware('auth')->group(function(){
+    Route::get('/my-account',[UserController::class,'index'])->name('user.index');
+});
+
+Route::middleware(['auth',\App\Http\Middleware\AuthAdmin::class])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
